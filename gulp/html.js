084c83ac7f304307bbd887e4a77ee059443b8f10
Patrick Connelly <patrick@deadlypenguin.com>
2021-10-25 15:27:43 -0400

    Adding test sponsorship page and team page

--- gulp/html.js
+++ gulp/html.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
 const gulp = require('gulp');
 const gulp_connect = require('gulp-connect');
+const gulp_data = require('gulp-data');
 const gulp_nunjucks = require('gulp-nunjucks-render');
 const path = require('path');
 
@@ -22,6 +24,26 @@ function template_path() {
 }
 
 /**
+ * Loads the JSON data
+ * @param {String} file The filename
+ * @returns {String} The json data
+ */
+function getJsonForFile(file) {
+    const extension = 'html';
+    const data_file = path.join(path.dirname(file.path), path.basename(file.path, '.' + extension) + '.json');
+
+    var data = {};
+
+    try {
+        data.data = JSON.parse(fs.readFileSync(data_file));
+    } catch (err) {
+        // It's ok if we don't have supplemental data
+    }
+
+    return data;
+}
+
+/**
  * Outputs the HTML
  * @returns {Promise} A promise for when the HTML has been outputted
  */
@@ -33,6 +55,7 @@ function html() {
     };
 
     return gulp.src(page_path())
+        .pipe(gulp_data(getJsonForFile))
         .pipe(gulp_nunjucks(nunjucks_config))
         .pipe(gulp.dest(constants.getDistDir()))
         .pipe(gulp_connect.reload());
