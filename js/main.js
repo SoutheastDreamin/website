55736b190a014e3ca72c6f2d05cbf1a9e05c6f6a
pcon <patrick@deadlypenguin.com>
2023-02-10 20:49:18 +0000

    Deploying to gh-pages from  @ 5b4ff3117130298b34d912f230b2a6e56724673e 🚀

--- js/main.js
+++ js/main.js
@@ -7,6 +7,46 @@ jQuery(document).ready(function () {
         jQuery('.navbar-menu').toggleClass('is-active');
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Functions to open and close a modal
+    /**
+     * Opens the modal
+     * @param {Object} $el The target
+     * @returns {undefined}
+     */
+    function openModal($el) {
+        $el.classList.add('is-active');
+    }
+
+    /**
+     * Closes the modal
+     * @param {Object} $el The target
+     * @returns {undefined}
+     */
+    function closeModal($el) {
+        $el.classList.remove('is-active');
+    }
+
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+
+        $trigger.addEventListener('click', () => {
+            openModal($target);
+        });
+    });
+
+    // Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+
+        $close.addEventListener('click', () => {
+            closeModal($target);
+        });
+    });
+});
 },{"jquery":2}],2:[function(require,module,exports){
 /*!
  * jQuery JavaScript Library v3.6.0
