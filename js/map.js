eedb0ca188c1231207bbc034fb7561d3403a4a96
Patrick Connelly <patrick@deadlypenguin.com>
2023-06-02 09:06:48 -0400

    Fixing map on schedule page

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 const jQuery = require('jquery');
 const L = require('leaflet');
 //const img_width = 876;
-const img_height = 902;
+const img_height = window.HOTEL_MAP_HEIGHT;
 
 const width = jQuery('#map').innerWidth() - 100;
 const height = img_height;
